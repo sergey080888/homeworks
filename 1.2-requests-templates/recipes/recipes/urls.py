c8833dc751c69a88b2a02efd4868a714df88a925
Сергей Ковальский <237264@mail.ru>
2022-10-24 18:27:06 +0300

    home work 1.2 recipes

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import handler
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<recipe>/', handler),
 ]
